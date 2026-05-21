5435cd79761c588ca5c09b9e74f18b1a61c04d2a
RosinaZ <137255370+Shampoopoooo@users.noreply.github.com>
2026-05-20 22:18:53 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Setup Multer for file uploads (in memory)
 const upload = multer({ storage: multer.memoryStorage() });
